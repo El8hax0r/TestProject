a78fffb3580c957e88faf30769a2940e5d66f1ea
Matthew Mitchell <39917204+El8hax0r@users.noreply.github.com>
2020-01-20 20:10:48 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise_02._2_CanRack_Optional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PurchasePrice sodaPrice = new PurchasePrice(35);
+
+            Console.WriteLine("Welcome to the .NET C# Soda Vending Machine");
+            Console.Write("Please insert {0} cents: ",sodaPrice.Price);
+            int valueInserted = int.Parse(Console.ReadLine());
+            Console.WriteLine("You have inserted {0} cents", valueInserted);
+            Console.WriteLine("Thanks, here is your soda.");
+        }
+    }
+}
---
+++ PurchasePrice.cs
@@ -0,0 +1,34 @@
+﻿namespace Exercise_02._2_CanRack_Optional
+{
+    //  This class represents the purchase price of something.
+    //  In our software project, we will use it to represent the price of
+    //  one can of soda.
+    public class PurchasePrice
+    {
+        private int price = 0;
+
+        // This constructor sets the purchase price to zero
+        public PurchasePrice()
+        {
+            price = 0; 
+        }
+        // This constructor allows a new purchase price to be set by the user
+        public PurchasePrice(int initialPrice)
+        {
+            price = initialPrice;
+        }
+        //  This property gets and sets the value the purchase price.
+        public int Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+    } //end PurchasePrice
+
+}
